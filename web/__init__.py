bdc792fac329a2697c135d7619232d3e50197510
Richard Hitier <hitier.richard@gmail.com>
2023-01-24 14:02:05 +0100

    New flask configuration imports from yaml

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from web.config import Config
 
 
-def create_app():
+def create_app(config_instance=None):
     app = Flask(__name__)
+
+    if not config_instance:
+        config_instance = Config()
+    app.config.from_object(config_instance)
 
     from web.main import bp as main_bp
     app.register_blueprint(main_bp)
