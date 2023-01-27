ccfdebe397cbcdb8877269a18090834a324862a9
Richard Hitier <hitier.richard@gmail.com>
2023-01-27 17:10:26 +0100

    Reformat code comments

--- bht/__main__.py
+++ bht/__main__.py
@@ -16,33 +16,37 @@ def run_dir(_base_pdf_dir):
         folders_or_pdf_path = os.path.join(_base_pdf_dir, folders_or_pdf)
         if folders_or_pdf.endswith(
                 ".pdf"):  # If '.pdf' on "Papers" folder --> paper not treated --> processing paper treatment.
-            os.makedirs(os.path.join(_base_pdf_dir, folders_or_pdf.replace(".pdf",
-                                                                           "")))  # create the directory under the same name than the paper.
-            shutil.move(folders_or_pdf_path, os.path.join(_base_pdf_dir, folders_or_pdf.replace(".pdf",
-                                                                                                "")))  # move '.pdf' to his directory.
+            # create the directory under the same name than the paper.
+            os.makedirs(os.path.join(_base_pdf_dir, folders_or_pdf.replace(".pdf", "")))
+            # move '.pdf' to his directory.
+            shutil.move(folders_or_pdf_path, os.path.join(_base_pdf_dir, folders_or_pdf.replace(".pdf", "")))
         pdf_paths = os.path.join(_base_pdf_dir, folders_or_pdf.replace(".pdf", ""))
         from bht.SUTime_processing import SUTime, SUTime_treatement, SUTime_transform
         sutime = SUTime(mark_time_ranges=True, include_range=True)  # load sutime wrapper
         for pdf_files in os.listdir(pdf_paths):  # processing treatment.
             if pdf_files.endswith(".pdf"):
                 print(pdf_paths)
-                if len(os.listdir(
-                        pdf_paths)) == 1:  # Only 1 file (the pdf) --> directory never treated --> processing first treatment.
+                # Only 1 file (the pdf) --> directory never treated --> processing first treatment.
+                if len(os.listdir(pdf_paths)) == 1:
                     PDF_file = os.path.join(pdf_paths, pdf_files)
                     PDF_OCRiser(pdf_paths, PDF_file)  # OCR the pdf file
-                    GROBID_generation(pdf_paths)  # generate the XML GROBID file
-                    ocr_filter(pdf_paths)  # filter result of the OCR to deletes references, change HHmm4 to HH:mm, etc ...
-                    SUTime_treatement(pdf_paths,
-                                      sutime)  # SUTime read all the file and save its results in a file "res_sutime.json"
-                    SUTime_transform(
-                        pdf_paths)  # transforms some results of sutime to complete missing, etc ... save results in "res_sutime_2.json"
-                    entities_finder(pdf_paths)  # entities recognition and association + writing of HPEvent
+                    # generate the XML GROBID file
+                    GROBID_generation(pdf_paths)
+                    # filter result of the OCR to deletes references, change HHmm4 to HH:mm, etc ...
+                    ocr_filter(pdf_paths)
+                    # SUTime read all the file and save its results in a file "res_sutime.json"
+                    SUTime_treatement(pdf_paths, sutime)
+                    # transforms some results of sutime to complete missing, etc ... save results in "res_sutime_2.json"
+                    SUTime_transform(pdf_paths)
+                    # entities recognition and association + writing of HPEvent
+                    entities_finder(pdf_paths)
                 else:  # case directory already treated: processing only after GROBID generation. (COMMENT TO DISABLE)
-                    ocr_filter(pdf_paths)  # filter result of the OCR to deletes references, change HHmm to HH:mm, etc ...
-                    SUTime_treatement(pdf_paths,
-                                      sutime)  # SUTime read all the file and save its results in a file "res_sutime.json"
-                    SUTime_transform(
-                        pdf_paths)  # transforms some results of sutime to complete missing, etc ... save results in "res_sutime_2.json"
+                    # filter result of the OCR to deletes references, change HHmm to HH:mm, etc ...
+                    ocr_filter(pdf_paths)
+                    # SUTime read all the file and save its results in a file "res_sutime.json"
+                    SUTime_treatement(pdf_paths, sutime)
+                    # transforms some results of sutime to complete missing, etc ... save results in "res_sutime_2.json"
+                    SUTime_transform(pdf_paths)
                     entities_finder(pdf_paths)  # entities recognition and association + writing of HPEvent
 
 
