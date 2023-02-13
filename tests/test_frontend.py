139f842fbb89bd4fd4216e287dabb4ceda96dbf2
Richard Hitier <hitier.richard@gmail.com>
2023-02-13 11:41:29 +0100

    Insert papers tests in class

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -1,9 +1,20 @@
-def test_papers_with_uploadfield(test_client):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/papers' page is requested (GET)
-    THEN check that the response is valid
-    """
-    response = test_client.get("/papers")
-    assert response.status_code == 200
-    assert b"Upload new File" in response.data
+class TestPapersPage:
+    def test_papers_uploadfield(self, test_client):
+        """
+        GIVEN a Flask application configured for testing
+        WHEN the '/papers' page is requested (GET)
+        THEN check that the response is valid
+        """
+        response = test_client.get("/papers")
+        assert response.status_code == 200
+        assert b"Upload new File" in response.data
+
+    def test_papers_pdffield(self, test_client):
+        """
+        GIVEN a Flask application configured for testing
+        WHEN the '/papers' page is requested (GET)
+        THEN check that the response has download pdf field
+        """
+        response = test_client.get("/papers")
+        assert response.status_code == 200
+        assert b"Upload from URL" in response.data
