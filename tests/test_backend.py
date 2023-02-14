d23ac53088ecf03a50e3be70db1449cfbf01bc65
Richard Hitier <hitier.richard@gmail.com>
2023-02-14 14:13:40 +0100

    Fix wrong os import

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,31 +1,29 @@
+import os
 import shutil
 import unittest
-
-import conftest
 
 from bht_config import yml_settings
 from bht.published_date_finder import published_date_finder
 from bht.GROBID_generator import GROBID_generation
+from tests.conftest import skip_slow_test
 
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.pdf_directory = yml_settings["BHT_PAPERS_DIR"]
-        test_pdf_file_orig = conftest.os.path.join(
+        test_pdf_file_orig = os.path.join(
             yml_settings["BHT_RESSOURCES_DIR"], "2016GL069787-test.pdf"
         )
-        self.test_pdf_file_dest = conftest.os.path.join(
+        self.test_pdf_file_dest = os.path.join(
             self.pdf_directory, "2016GL069787-test.pdf"
         )
         shutil.copy(test_pdf_file_orig, self.test_pdf_file_dest)
-        self.test_tei_file = conftest.os.path.join(
-            self.pdf_directory, "2016GL069787.tei.xml"
-        )
+        self.test_tei_file = os.path.join(self.pdf_directory, "2016GL069787.tei.xml")
 
     def tearDown(self):
         for file in [self.test_pdf_file_dest, self.test_tei_file]:
-            if conftest.os.path.isfile(file):
-                conftest.os.remove(file)
+            if os.path.isfile(file):
+                os.remove(file)
 
 
 class AdsTestCase(BaseTestCase):
@@ -38,8 +36,8 @@ class AdsTestCase(BaseTestCase):
         self.assertEqual("2017-04-01T00:00:00Z", found_date)
 
 
-@conftest.skip_slow_test
+@skip_slow_test
 class GrobidTestCase(BaseTestCase):
     def test_grobid_generation(self):
         GROBID_generation(self.pdf_directory)  # generate the XML GROBID file
-        self.assertTrue(conftest.os.path.isfile(self.test_tei_file))
+        self.assertTrue(os.path.isfile(self.test_tei_file))
