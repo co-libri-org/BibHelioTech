acc79ca23347583772b9e606209fabd327bad001
Richard Hitier <hitier.richard@gmail.com>
2023-03-06 10:35:30 +0100

    Add catalogs page in menu

--- tests/unit/test_frontend.py
+++ tests/unit/test_frontend.py
@@ -8,6 +8,18 @@ from web.main.routes import get_file_from_url, save_to_db
 from web.models import Paper
 
 
+class TestCatalogs:
+    def test_catalog_page(self, test_client):
+        """
+        GIVEN a flask app
+        WHEN the catalog page is requested
+        THEN check that page is returned
+        """
+        response = test_client.get("/catalogs")
+        assert response.status_code == 200
+        assert b"Catalogs by Mission" in response.data
+
+
 class TestFrontUtils:
     @skip_slow_test
     def test_get_file_from_url(self):
