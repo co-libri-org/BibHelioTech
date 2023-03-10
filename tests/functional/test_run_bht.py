57986292c997b7d95e3613bb67741f0cfbd0d069
Richard Hitier <hitier.richard@gmail.com>
2023-03-10 18:37:30 +0100

    Move run_bht tests

--- tests/functional/test_run_bht.py
+++ tests/functional/test_run_bht.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-from tests.functionnal.conftest import skip_selenium
+from tests.functional.conftest import skip_selenium
 from web.models import Paper
 
 
