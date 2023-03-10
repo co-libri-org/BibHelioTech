19d3f86ddd90378db0d5cddf9c94eedbd3269c28
Richard Hitier <hitier.richard@gmail.com>
2023-03-10 16:59:08 +0100

    Move TestCatTools

--- tests-to-refactor/conftest.py
+++ tests-to-refactor/conftest.py
@@ -3,25 +3,8 @@ import pytest
 from web import db
 
 
-@pytest.fixture(scope="function", autouse=True)
-def fresh_db():
-    # db_file = current_app.config["SQLALCHEMY_DATABASE_URI"].split("sqlite:///")[1]
-    # if os.path.exists(db_file):
-    #     os.remove(db_file)
-    # yield db
-    # db.session.rollback()
-    # db.session.drop_all()
-    # db.session.close()
-    pass
-
-
 @pytest.fixture(scope="function")
 def hpevents_in_db(hpevents_list):
     for event in hpevents_list:
         db.session.add(event)
         db.session.commit
-
-
-@pytest.fixture(scope="module")
-def test_client(app):
-    yield app.test_client()
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,4 +1,15 @@
-from web.models import Paper, Catalog, HpEvent, Doi, Mission, Instrument, Region
+from web.models import (
+    Paper,
+    Catalog,
+    HpEvent,
+    Doi,
+    Mission,
+    Instrument,
+    Region,
+    catfile_to_rows,
+    catfile_to_db,
+    rows_to_catstring,
+)
 
 
 class TestDb:
@@ -153,3 +164,19 @@ class TestModels:
         """
         region = Region(name="region-name")
         assert region.name == "region-name"
+
+
+class TestCatTools:
+    def test_catfile_to_rows(self, cat_for_test):
+        hp_events = catfile_to_rows(cat_for_test)
+        assert len(hp_events) == 46
+
+    def test_catfile_to_db(self, cat_for_test, db):
+        catfile_to_db(cat_for_test)
+        allevents = db.session.query(HpEvent).all()
+        assert len(allevents) == 46
+
+    def test_rows_to_catstring(self, cat_for_test):
+        hp_events = catfile_to_rows(cat_for_test)
+        cat_str = rows_to_catstring(hp_events, "what")
+        assert len(cat_str) == 6900
