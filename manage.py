15053de0782f1e60909267b9a73acec98aa1dc43
Richard Hitier <hitier.richard@gmail.com>
2024-01-02 11:23:34 +0100

    Fix deprecated call

--- manage.py
+++ manage.py
@@ -167,9 +167,8 @@ def feed_catalog(catalog_file):
 def run_worker():
     redis_url = current_app.config["REDIS_URL"]
     redis_connection = redis.from_url(redis_url)
-    with Connection(redis_connection):
-        worker = Worker(current_app.config["QUEUES"])
-        worker.work()
+    worker = Worker(current_app.config["QUEUES"], connection=redis_connection)
+    worker.work()
 
 
 if __name__ == "__main__":
