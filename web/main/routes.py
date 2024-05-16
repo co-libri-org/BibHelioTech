19d0a4e712ff0e9b022abedc0123b2a0f2ab2771
Richard Hitier <hitier.richard@gmail.com>
2024-05-16 16:35:28 +0200

    Read pipeline_verion from file now

--- web/main/routes.py
+++ web/main/routes.py
@@ -712,6 +712,7 @@ def api_catalogs_txt():
     catalog_txt_stream = rows_to_catstring(events_list, mission.name)
     date_now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     bht_pipeline_version = current_app.config["BHT_PIPELINE_VERSION"]
+    # TODO: build catalog name else where, may be from some bht.method()
     file_name = f"{mission.name}_{date_now}_bibheliotech_V{bht_pipeline_version}.txt"
     upload_dir = current_app.config["WEB_UPLOAD_DIR"]
     if not os.path.exists(upload_dir):
