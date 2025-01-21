1cabbb7406ff576ee4df5d4dd236e034275d6504
Richard Hitier <hitier.richard@gmail.com>
2025-01-21 10:59:42 +0100

    Refactor cli commands to directly call view funcs

--- manage.py
+++ manage.py
@@ -237,55 +237,34 @@ def paper_run(paper_id):
     except Exception as e:
         print(f"Couldn't run on paper #{paper_id}")
 
+
 @cli.command("paper_web_run")
 @click.argument("paper_id", required=True)
-@click.option("-h", "--host-name", required=True, help="Hostname")
-@click.option("-p", "--port-number", required=True, help="Port number")
-@click.option("-U", "--user-name", required=True, help="Auth username")
-@click.option("-P", "--password", required=True, help="Auth password")
-def paper_web_run_cmd(paper_id, host_name, port_number, user_name, password):
+def paper_web_run_cmd(paper_id):
     """Run the latest pipeline on that paper's article through web/RQ"""
-    with  current_app.test_request_context(), current_app.app_context():
-        url_path = url_for('main.bht_run', paper_id=paper_id, file_type="txt", _external=False)
-    run_url = f"http://{host_name}:{port_number}{url_path}"
-    # Send GET request to the URL
-    import requests
-    from requests.auth import HTTPBasicAuth
-    response = requests.get(run_url, auth=HTTPBasicAuth(user_name, password))
-
-    # Check if the request was successful
-    if response.status_code == 202:
-        # Parse the JSON response
-        data = response.json()
-        print(data)  # The JSON content will be in 'data'
-    else:
-        print(f"Failed to retrieve data at {run_url}. Status code: {response.status_code}")
-
+    with current_app.test_request_context(), current_app.app_context():
+        from web.main.routes import bht_run
+        response, status_code = bht_run(paper_id=paper_id, file_type="txt")
+        if status_code == 202:
+            data = response.json  # La réponse Flask contient déjà le JSON
+            print(data)
+        else:
+            print(f"Failed to process paper {paper_id}. Status code: {status_code}")
 
 
 @cli.command("paper_web_status")
 @click.argument("paper_id", required=True)
-@click.option("-h", "--host-name", required=True, help="Hostname")
-@click.option("-p", "--port-number", required=True, help="Port number")
-@click.option("-U", "--user-name", required=True, help="Auth username")
-@click.option("-P", "--password", required=True, help="Auth password")
-def paper_web_status_cmd(paper_id, host_name, port_number, user_name, password):
+def paper_web_status_cmd(paper_id):
     """Get the running status on that paper's article through web/RQ"""
-    with  current_app.test_request_context(), current_app.app_context():
-        url_path = url_for('main.bht_status', paper_id=paper_id, _external=False)
-    status_url = f"http://{host_name}:{port_number}{url_path}"
-    # Send GET request to the URL
-    import requests
-    from requests.auth import HTTPBasicAuth
-    response = requests.get(status_url, auth=HTTPBasicAuth(user_name, password))
+    with current_app.test_request_context(), current_app.app_context():
+        from web.main.routes import bht_status
+        response, status_code = bht_status(paper_id=paper_id)
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-        print(data)  # The JSON content will be in 'data'
-    else:
-        print(f"Failed to retrieve data at {status_url}. Status code: {response.status_code}")
+        if status_code == 200:
+            data = response.json
+            print(data)
+        else:
+            print(f"Failed to get status for paper {paper_id}. Status code: {status_code}")
 
 
 @cli.command("paper_run")
