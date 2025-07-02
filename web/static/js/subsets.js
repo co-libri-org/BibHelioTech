14ca29b4d74ec1a9c214bafb1ba23e79196317c0
Richard Hitier <hitier.richard@gmail.com>
2025-07-02 18:28:13 +0200

    Move display logic from ajax to callback method

--- web/static/js/subsets.js
+++ web/static/js/subsets.js
@@ -10,77 +10,62 @@ function failedToStatus(err, statusElmtId, message) {
     console.error(err)
 }
 
-function toggleSubsetDisplay(subset_name, state){
+function toggleSubsetDisplay(subset_name, task_status, subset_status, message, alt_message){
 
-    const subsetBtnElmtId = '#show-' + subset_name;
-    const unzipBtnElmtId = '#unzip-' + subset_name;
-    const statusElmtId = '#zip-status-' + subset_name;
-    const spinnerElmtId = '#spinner-' + subset_name;
+    const subsetTr = $(`#${subset_name}`);
+    const showLink = subsetTr.find(".show-subset");
+    const unzipBtn = subsetTr.find(".unzip-subset");
+    const statusMsg = subsetTr.find(".zip-status");
+    const statusSpinner = subsetTr.find(".status-spin");
 
-    if (state  == 'extracted'){
-        $(subsetBtnElmtId).removeClass('disabled')
-    }
-    else if (state == 'zipped' ) {
-        $(unzipBtnElmtId).removeClass('disabled')
-    }
-    else if (state == 'running' ) {
-        // show spinner while running task
-        $(spinnerElmtId).removeClass('d-none').addClass('d-inline-block')
-    }
-    else if (state == 'reset' ) {
-        // Disable all
-        $(subsetBtnElmtId).addClass('disabled')
-        $(unzipBtnElmtId).addClass('disabled')
-        $(spinnerElmtId).removeClass('d-inline-block').addClass('d-none')
-    }
-    //if
+    showLink.addClass("disabled");
+    unzipBtn.addClass("disabled");
+    statusMsg.text(task_status+" World");
 }
-// url_for template for dynamically use before subset_name is known
-const urlSubsetStatusTemplate = "{{ url_for('main.api_subset_status', subset_name='__subset_name__') }}";
 
-function updateSubsetStatus(subset_name){
-    console.log("Updating status for "+subset_name);
+
+function updateSubsetStatus(subsetName){
+    console.log("Updating status for "+subsetName);
    // now dynamically build the flask url
-   const url = urlSubsetStatusTemplate.replace("__subset_name__", subset_name);
-   const statusElmtId = '#zip-status-' + subset_name;
-
-   toggleSubsetDisplay(subset_name, 'reset');
+   const url = urlSubsetStatusTemplate.replace("__subset_name__", subsetName);
 
    $.ajax({
             url: url,
             method: 'GET',
         })
    .done((res) => {
+        const subsetStatus = res.data.subset_status;
         const taskStatus = res.data.task_status;
         const taskProgress = res.data.task_progress;
+        const subsetMessage = res.data.message;
+        const altMessage = res.data.alt_message;
 
 
         if (taskStatus == "started") {
-            $(statusElmtId).html(taskProgress).attr("title", res.data.alt_message);
-            toggleSubsetDisplay(subset_name, 'running');
+            // update and wait for task to finish
             setTimeout(function() {
-                updateSubsetStatus(subset_name);
+                updateSubsetStatus(subsetName);
             }, 500);
         } else if (taskStatus == "queued") {
-            toggleSubsetDisplay(subset_name, 'reset');
-            $(statusElmtId).html(taskStatus)
-                               .attr("title", res.data.alt_message);
-            // wait for queued to be executed
+            // update and wait for task to be executed
             setTimeout(function() {
-                updateSubsetStatus(subset_name);
+                updateSubsetStatus(subsetName);
             }, 1000);
-        } else if (taskStatus == "finished") {
-            // update display
-            toggleSubsetDisplay(subset_name, 'extracted');
-            $(statusElmtId).html("Finished")
-                               .attr("title", res.data.alt_message);
-        } else {
-            $(statusElmtId).html(res.data.message)
-                               .attr("title", res.data.alt_message);
         }
+
+        let normalizedStatus = taskStatus;
+        if (res.status === "error" && altMessage.includes("Redis")) {
+            normalizedStatus = "CnxError";
+        } else if (res.status === "failed" && altMessage.includes("No task id")) {
+            normalizedStatus = "NoJob";
+        }
+
+       toggleSubsetDisplay(subsetName, normalizedStatus, subsetStatus, subsetMessage, altMessage)
+
    })
    .fail((err) => {
-        failedToStatus(err, statusElmtId, "FAILED Update Unzip Status");
+        console.error("Error", err);
+        // TODO: call fallback
    });
 }
 
