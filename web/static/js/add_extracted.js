4e31fa4e9183cd19ac6f50b83843294c87466de8
Richard Hitier <hitier.richard@gmail.com>
2025-07-10 08:55:42 +0200

    Move btn.add-extracted.onclick() code to function

--- web/static/js/add_extracted.js
+++ web/static/js/add_extracted.js
@@ -19,30 +19,34 @@ function updateExtractedTr(istex_struct){
     // in last <td> remove <button> replace with text "added"
     const $lastTd = $tr.find('td').last();
     $lastTd.empty().text(istex_struct.status);
-    
+
     // move the page to that <tr>
     $('html, body').animate({
         scrollTop: $tr.offset().top - 100 // offset for visibility
     }, 500);
 }
 
+function handleAddExtracted(subset_name, istex_id){
+    $.ajax({
+        url: urlAddExtracted,
+        method: "POST",
+        data: JSON.stringify({ subset_name: subset_name, istex_id: istex_id, exec_type: "mocked" }),
+        contentType: 'application/json; charset=utf-8'
+    })
+    .done((res) => {
+        updateExtractedTr(res.data.istex_struct);
+    })
+    .fail((err) => {
+        //const statusElmtId = '#zip-status-' + subset_name;
+        //failedToStatus(err, statusElmtId, "FAILED Unzip Subset");
+        console.error(err);
+    });
+}
+
 function setAddBtnOnClick(){
     $('.add-extracted').click(function(){
-        const subset_name = $(this).data("subset_name")
-        const istex_id = $(this).data("istex_id")
-        $.ajax({
-            url: urlAddExtracted,
-            method: "POST",
-            data: JSON.stringify({ subset_name: subset_name, istex_id: istex_id, exec_type: "mocked" }),
-            contentType: 'application/json; charset=utf-8'
-        })
-        .done((res) => {
-            updateExtractedTr(res.data.istex_struct);
-        })
-        .fail((err) => {
-            //const statusElmtId = '#zip-status-' + subset_name;
-            //failedToStatus(err, statusElmtId, "FAILED Unzip Subset");
-            console.error(err);
-        });
+        const subset_name = $(this).data("subset_name");
+        const istex_id = $(this).data("istex_id");
+        handleAddExtracted(subset_name, istex_id);
     });
 }
