ef68b9065dd6a77debe03a3595508ca96d6b8913
Richard Hitier <hitier.richard@gmail.com>
2025-07-10 08:30:49 +0200

    Update the paper's Tr at api_add_extracted call

--- web/static/js/add_extracted.js
+++ web/static/js/add_extracted.js
@@ -1,6 +1,24 @@
 function updateExtractedTr(istex_struct){
-    alert(JSON.stringify(istex_struct));
+    // get <tr> by id==istex_id
+    const $tr = $('#'+istex_struct.istex_id);
+    if ($tr.length === 0){
+        console.error("TR not found for istex_id:", istex_struct.istex_id);
+        return;
+    }
+    // change class "to_add" in "added" or "mocked" by istex_struct.status
+    $tr.removeClass("to_add").addClass(istex_struct.status);
+    // insert <a class="btn"> in first <th>  with inner_text=istex_struct.paper_id and href="#"
+    $firstTh = $tr.find('th.td_num');
+    $firstTh.html(`<a class="btn btn-warning" href="#" title="Show paper #${istex_struct.paper_id}">${istex_struct.paper_id}</a>`);
+    // in last <td> remove <button> replace with text "added"
+    const $lastTd = $tr.find('td').last();
+    $lastTd.empty().text(istex_struct.status);
+    // move the page to that <tr>
+    $('html, body').animate({
+        scrollTop: $tr.offset().top - 100 // offset for visibility
+    }, 500);
 }
+
 function setAddBtnOnClick(){
     $('.add-extracted').click(function(){
         const subset_name = $(this).data("subset_name")
